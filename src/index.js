1bf80b19cf61eb1f4115be102d28cf455137c1fd
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-22 18:27:59 +0300

    fix bug cors

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-// import { fromEvent, Observable } from 'rxjs'
-// import { map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators'
-// import { ajax } from 'rxjs/ajax'
+import { fromEvent, Observable } from 'rxjs'
+import { map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators'
+import { ajax } from 'rxjs/ajax'
 
-// const url = 'http://api.github.com/search/users?q='
+const url = 'https://api.github.com/search/users?q='
 
-// const search = document.getElementById('search')
+const search = document.getElementById('search')
 
-// const stream$ = fromEvent(search, 'input')
-//   .pipe(
-//     map(e => e.target.value),
-//     debounceTime(1000),
-//     distinctUntilChanged(),
-//     switchMap(v => {
-//       return ajax.getJSON(url + v)
-//     })
-//   )
+const stream$ = fromEvent(search, 'input')
+  .pipe(
+    map(e => e.target.value),
+    debounceTime(1000),
+    distinctUntilChanged(),
+    switchMap(v => {
+      return ajax.getJSON(url + v)
+    })
+  )
 
-// stream$.subscribe(value => {
-//   console.log(value)
-// })
+stream$.subscribe(value => {
+  console.log(value)
+})
 
-import { ajax } from 'rxjs/ajax';
-import { map, catchError, of } from 'rxjs';
+// import { ajax } from 'rxjs/ajax';
+// import { map, catchError, of } from 'rxjs';
 
-const obs$ = ajax.getJSON('https://api.github.com/users?per_page=5').pipe(
-  map(userResponse => console.log('users: ', userResponse)),
-  catchError(error => {
-    console.log('error: ', error);
-    return of(error);
-  })
-);
+// const obs$ = ajax.getJSON('https://api.github.com/users?per_page=5').pipe(
+//   map(userResponse => console.log('users: ', userResponse)),
+//   catchError(error => {
+//     console.log('error: ', error);
+//     return of(error);
+//   })
+// );
 
-obs$.subscribe({
-  next: value => console.log(value),
-  error: err => console.log(err)
-});
+// obs$.subscribe({
+//   next: value => console.log(value),
+//   error: err => console.log(err)
+// });
