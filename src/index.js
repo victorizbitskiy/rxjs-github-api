1bcb007340896b526c877e36034fd9951c5f37ee
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-22 20:19:51 +0300

    add cards of GitHub users

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import { fromEvent, Observable } from 'rxjs'
-import { map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators'
+import { EMPTY, fromEvent, Observable } from 'rxjs'
+import { map, debounceTime, distinctUntilChanged, switchMap, mergeMap, tap, catchError, filter } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax'
 
 const url = 'https://api.github.com/search/users?q='
 
 const search = document.getElementById('search')
+const result = document.getElementById('result')
 
 const stream$ = fromEvent(search, 'input')
   .pipe(
     map(e => e.target.value),
     debounceTime(1000),
     distinctUntilChanged(),
-    switchMap(v => {
-      return ajax.getJSON(url + v)
-    })
+    tap(() => result.innerHTML = ''),
+    filter(v => v.trim()),
+    switchMap(v => ajax.getJSON(url + v).pipe(
+      catchError(err => EMPTY)
+    )),
+    map(response => response.items),
+    mergeMap(items => items)
   )
 
-stream$.subscribe(value => {
-  console.log(value)
+stream$.subscribe(user => {
+  const html = `
+  <div class="card">
+  <div class="card-image">
+    <img src="${user.avatar_url}" />
+    <span class="card-title">${user.login}</span>
+  </div>
+  <div class="card-action">
+    <a href="${user.html_url}" target="_blank">Открыть GitHub</a>
+  </div>
+</div>
+`
+  result.insertAdjacentHTML('beforeend', html)
 })
-
-// import { ajax } from 'rxjs/ajax';
-// import { map, catchError, of } from 'rxjs';
-
-// const obs$ = ajax.getJSON('https://api.github.com/users?per_page=5').pipe(
-//   map(userResponse => console.log('users: ', userResponse)),
-//   catchError(error => {
-//     console.log('error: ', error);
-//     return of(error);
-//   })
-// );
-
-// obs$.subscribe({
-//   next: value => console.log(value),
-//   error: err => console.log(err)
-// });
